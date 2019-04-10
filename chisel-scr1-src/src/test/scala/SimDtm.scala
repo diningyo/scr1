99cd468ed9bcdc231b16b52d24f7b7e89919bc94
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-04-10 23:25:52 +0900

    add testbench top module file.

---
+++ chisel-scr1-src/src/test/scala/SimDtm.scala
@@ -0,0 +1,22 @@
+
+import chisel3._
+
+import scr1._
+
+class SimDtm(implicit cfg: SCR1Config) extends Module {
+  val io = IO(new Bundle {
+
+  })
+
+  val memSize = 32 * 1024
+  val numIf = 2
+  val addrWidth = 32
+  val dataWidth = 32
+  val idWidth = 4
+
+  val dut = Module(new Scr1Top)
+  val mem = Module(new Scr1MemoryTbAxiWrapper(memSize, numIf, addrWidth, dataWidth, idWidth))
+
+  io := DontCare
+  dut.io := DontCare
+}
