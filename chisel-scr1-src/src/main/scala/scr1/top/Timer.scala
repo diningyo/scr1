b6a02494fbee8d40c49d48307622dd6906862f78
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-04-20 23:21:26 +0900

    add Timer module (and it only has I/O port)

---
+++ chisel-scr1-src/src/main/scala/scr1/top/Timer.scala
@@ -0,0 +1,29 @@
+// See LICENSE for license details.
+package scr1.top
+
+import chisel3._
+import chisel3.core.{Mux, withClockAndReset}
+import chisel3.util.{Cat, MuxCase}
+import scr1.core._
+
+class Timer(implicit cfg: SCR1Config) extends Module {
+  val io = IO(new Bundle {
+    // Common
+    val rtcClk = Input(Clock())
+    val rtcRst = Input(Bool())
+
+    // Memory interface
+    val dmemReq = Input(Bool())
+    val dmemCmd = Input(UInt(2.W))
+    val dmemWidth = Input(UInt(2.W))
+    val dmemAddr = Input(UInt(cfg.dmemAWidth.W))
+    val dmemWdata = Input(UInt(cfg.dmemDWidth.W))
+    val dmemReqAck = Output(Bool())
+    val dmemRdata = Output(UInt(cfg.dmemDWidth.W))
+    val dmemResp = Output(UInt(2.W))
+
+    // Timer interface
+    val timerVal = Output(UInt(64.W))
+    val timerIrq = Output(Bool())
+  })
+}
